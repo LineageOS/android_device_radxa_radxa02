5e5a19b7c70929e271a0e45233a5686610c2a79b
Bruno Martins <bgcngm@gmail.com>
2026-08-21 16:17:13 +0100

    radxa02: rro_overlays: Assign generic name to RRO overlay packages
    Change-Id: I9495b94210f0c92f1204ede7d5d0a527f67a4bc8

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 
 ## Bluetooth
 PRODUCT_PACKAGES += \
-    RadxaZero2BluetoothOverlay \
+    BluetoothOverlayTarget \
     libbt-vendor
 
 $(call soong_config_set,brcm_libbt,bdroid_buildcfg_include_dir,$(LOCAL_PATH)/bluetooth/include)
